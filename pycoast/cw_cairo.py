6be9312a3f02836fb817a1d4c4c314f0a7d178f4
Adam.Dybbroe <a000680@c20671.ad.smhi.se>
2016-06-16 09:18:04 +0200

    Fix cairo to pil and pil to cairo
    Signed-off-by: Adam.Dybbroe <a000680@c20671.ad.smhi.se>

--- pycoast/cw_cairo.py
+++ pycoast/cw_cairo.py
@@ -24,6 +24,7 @@
 # You should have received a copy of the GNU General Public License
 # along with this program.  If not, see <http://www.gnu.org/licenses/>.
 
+import pdb
 
 import numpy as np
 from PIL import Image
@@ -39,6 +40,7 @@ logger = logging.getLogger(__name__)
 #     logger.error('cairocffi is needed')
 #     raise
 
+
 from cairo import FORMAT_ARGB32, Context, ImageSurface
 
 from .cw_base import ContourWriterBase
@@ -49,6 +51,14 @@ class CairoDrawObject(object):
     def __init__(self, context, pilimage):
         self.context = context
         self.pilimage = pilimage
+
+
+# Doesn't work with cairo, but works fine with cairocffi
+# class CustomImageSurface(ImageSurface):
+
+#     def __init__(self):
+#         ImageSurface.__init__(self)
+#         self.size = (self.get_width(), self.get_height())
 
 
 class ContourWriterCairo(ContourWriterBase):
@@ -66,15 +76,12 @@ class ContourWriterCairo(ContourWriterBase):
     # they are not fully compatible)
 
     # Map the color from string to R, G, B
-    colormap = {'white': (1, 1, 1),
-                'blue': (0, 0, .9),
+    colormap = {'white': (255, 255, 255),
+                'blue': (0, 0, 255),
                 'black': (0, 0, 0),
-                'grey': (.7, .7, .7),
-                'red': (.9, 0, 0),
-                'green': (0, .9, 0)}
-
-    # def __init__(self):
-    #    ContourWriterBase.__init__(self)
+                'grey': (179, 179, 179),
+                'red': (255, 0, 0),
+                'green': (0, 255, 0)}
 
     def _get_canvas(self, image):
         """Returns PIL draw canvass, convert from Cairo image context if
@@ -84,18 +91,24 @@ class ContourWriterCairo(ContourWriterBase):
         if isinstance(image, Context):
             # Create PIL image from cairo context:
             raise NotImplementedError(
-                'Cairo context as input not yet supported!')
+                'Cairo context as input not supported!')
+        elif isinstance(image, ImageSurface):
+            # Create PIL image from cairo image surface:
+            self._surface = image
+            pil_img = self._cairo_to_pil()
+            canvas = CairoDrawObject(Context(image), pil_img)
+            return canvas
 
         elif isinstance(image, Image.Image):
-            fmt = FORMAT_ARGB32
-            width, height = image.size
-            data = image.convert('RGBA')
-            data = np.asarray(data)
-            tmp = data.copy()
-            self._surface = ImageSurface.create_for_data(tmp, FORMAT_ARGB32,
-                                                         width, height)
+            self._surface = _pil_to_cairo(image)
+            # fmt = FORMAT_ARGB32
+            # width, height = image.size
+            # data = image.convert('RGBA')
+            # data = np.asarray(data)
+            # tmp = data.copy()
+            # self._surface = ImageSurface.create_for_data(tmp, FORMAT_ARGB32,
+            #                                              width, height)
             canvas = CairoDrawObject(Context(self._surface), image)
-
             return canvas
         else:
             raise ValueError("Unsupported image format.")
@@ -119,13 +132,10 @@ class ContourWriterCairo(ContourWriterBase):
         """Draw polygon
         """
 
+        alpha = kwargs.get('outline_opacity', 255)
+        alpha /= 255.
         outline = kwargs.get('outline')
-        if type(outline) is str:
-            if outline not in self.colormap:
-                logger.warning("Color %s not supported", outline)
-            r, g, b = self.colormap.get(outline, (1, 1, 1))
-        else:
-            r, g, b = outline
+        r, g, b = get_rgb_values(outline, self.colormap)
 
         img_ctx = draw_obj.context
 
@@ -134,7 +144,7 @@ class ContourWriterCairo(ContourWriterBase):
         for x, y in coordinates[1:]:
             img_ctx.line_to(x, y)
 
-        img_ctx.set_source_rgb(r, g, b)  # Solid color
+        img_ctx.set_source_rgba(r, g, b, alpha)  # Solid color
         img_ctx.set_line_width(kwargs['width'])
         img_ctx.stroke()
 
@@ -207,13 +217,11 @@ class ContourWriterCairo(ContourWriterBase):
         """Finish the draw object (contains cairo image context object)
         """
         img_ctx = draw_obj.context
-
         img = self._cairo_to_pil()
         draw_obj.pilimage.paste(img)
 
-        draw_obj.pilimage.save('/tmp/kurt.png')
-
-        self._surface.write_to_png('/tmp/cairo_surface.png')
+        # draw_obj.pilimage.save('/tmp/kurt.png')
+        # self._surface.write_to_png('/tmp/cairo_surface.png')
 
     def add_shapefile_shapes(self, img_ctx, area_def, filename,
                              feature_type=None, fill=None, fill_opacity=255,
@@ -369,7 +377,7 @@ class ContourWriterCairo(ContourWriterBase):
                           fill_opacity=fill_opacity, outline=outline,
                           width=width, outline_opacity=outline_opacity)
 
-    def add_grid(self, img_ctx, area_def, (Dlon, Dlat), (dlon, dlat),
+    def add_grid(self, img_surf, area_def, (Dlon, Dlat), (dlon, dlat),
                  font=None, write_text=True, fill=None, fill_opacity=255,
                  outline='white', width=1, outline_opacity=255,
                  minor_outline='white', minor_width=0.5,
@@ -407,7 +415,7 @@ class ContourWriterCairo(ContourWriterBase):
         minor_is_tick : boolean, optional
             Use tick minor line style (True) or full minor line style (False)
         """
-        self._add_grid(img_ctx, area_def, Dlon, Dlat, dlon, dlat,
+        self._add_grid(img_surf, area_def, Dlon, Dlat, dlon, dlat,
                        font=font, write_text=write_text,
                        fill=fill, fill_opacity=fill_opacity, outline=outline,
                        width=width, outline_opacity=outline_opacity,
@@ -456,8 +464,9 @@ class ContourWriterCairo(ContourWriterBase):
             Use tick minor line style (True) or full minor line style (False)
         """
 
-        img_ctx = _read_image(filename)
-        self.add_grid(img_ctx, area_def, (Dlon, Dlat), (dlon, dlat),
+        img_surface = _read_image(filename)
+
+        self.add_grid(img_surface, area_def, (Dlon, Dlat), (dlon, dlat),
                       font=font, write_text=write_text,
                       fill=fill, fill_opacity=fill_opacity,
                       outline=outline, width=width,
@@ -467,11 +476,11 @@ class ContourWriterCairo(ContourWriterBase):
                       minor_outline_opacity=minor_outline_opacity,
                       minor_is_tick=minor_is_tick,
                       lon_placement=lon_placement, lat_placement=lat_placement)
-        _save_image(filename, img_ctx)
+        _save_image(filename, img_surface)
 
-    def add_coastlines(self, img_ctx, area_def, resolution='c', level=1,
+    def add_coastlines(self, img_surface, area_def, resolution='c', level=1,
                        fill=None, fill_opacity=255, outline='white', width=1,
-                       outline_opacity=255, x_offset=0, y_offset=0):
+                       outline_opacity=255, x_offset=0, y_offset=0, **kwargs):
         """Add coastlines to a PIL image object
 
         :Parameters:
@@ -501,17 +510,17 @@ class ContourWriterCairo(ContourWriterBase):
             Pixel offset in y direction
         """
 
-        self._add_feature(img_ctx, area_def, 'polygon', 'GSHHS',
+        self._add_feature(img_surface, area_def, 'polygon', 'GSHHS',
                           resolution=resolution, level=level,
                           fill=fill, fill_opacity=fill_opacity,
                           outline=outline, width=width,
                           outline_opacity=outline_opacity, x_offset=x_offset,
-                          y_offset=y_offset)
+                          y_offset=y_offset, **kwargs)
 
     def add_coastlines_to_file(self, filename, area_def, resolution='c',
                                level=1, fill=None, fill_opacity=255,
                                outline='white', width=1, outline_opacity=255,
-                               x_offset=0, y_offset=0):
+                               x_offset=0, y_offset=0, **kwargs):
         """Add coastlines to an image file
 
         :Parameters:
@@ -531,7 +540,7 @@ class ContourWriterCairo(ContourWriterBase):
             Opacity of land color
         outline : str or (R, G, B), optional
             Coastline color
-        width : float, optional
+        width : float, o ptional
             Width of coastline
         outline_opacity : int, optional {0; 255}
             Opacity of coastline color
@@ -541,17 +550,17 @@ class ContourWriterCairo(ContourWriterBase):
             Pixel offset in y direction
         """
 
-        img_ctx = _read_image(filename)
-        self.add_coastlines(img_ctx, area_def, resolution=resolution,
+        img_surf = _read_image(filename)
+        self.add_coastlines(img_surf, area_def, resolution=resolution,
                             level=level, fill=fill,
                             fill_opacity=fill_opacity, outline=outline,
                             width=width, outline_opacity=outline_opacity,
-                            x_offset=x_offset, y_offset=y_offset)
-        _save_image(filename, img_ctx)
+                            x_offset=x_offset, y_offset=y_offset, **kwargs)
+        _save_image(filename, img_surf)
 
-    def add_borders(self, img_ctx, area_def, resolution='c', level=1,
+    def add_borders(self, img_surf, area_def, resolution='c', level=1,
                     outline='white', width=1, outline_opacity=255,
-                    x_offset=0, y_offset=0):
+                    x_offset=0, y_offset=0, **kwargs):
         """Add borders to a PIL image object
 
         :Parameters:
@@ -577,14 +586,15 @@ class ContourWriterCairo(ContourWriterBase):
             Pixel offset in y direction
         """
 
-        self._add_feature(img_ctx, area_def, 'line', 'WDBII', tag='border',
+        self._add_feature(img_surf, area_def, 'line', 'WDBII', tag='border',
                           resolution=resolution, level=level, outline=outline,
                           width=width, outline_opacity=outline_opacity,
-                          x_offset=x_offset, y_offset=y_offset)
+                          x_offset=x_offset, y_offset=y_offset, **kwargs)
 
     def add_borders_to_file(self, filename, area_def, resolution='c',
                             level=1, outline='white', width=1,
-                            outline_opacity=255, x_offset=0, y_offset=0):
+                            outline_opacity=255, x_offset=0, y_offset=0,
+                            **kwargs):
         """Add borders to an image file
 
         :Parameters:
@@ -610,16 +620,16 @@ class ContourWriterCairo(ContourWriterBase):
             Pixel offset in y direction
         """
 
-        img_ctx = _read_image(filename)
-        self.add_borders(img_ctx, area_def, resolution=resolution, level=level,
+        img_surf = _read_image(filename)
+        self.add_borders(img_surf, area_def, resolution=resolution, level=level,
                          outline=outline, width=width,
                          outline_opacity=outline_opacity, x_offset=x_offset,
-                         y_offset=y_offset)
-        _save_image(filename, image)
+                         y_offset=y_offset, **kwargs)
+        _save_image(filename, img_surf)
 
-    def add_rivers(self, img_ctx, area_def, resolution='c', level=1,
+    def add_rivers(self, img_surf, area_def, resolution='c', level=1,
                    outline='white', width=1, outline_opacity=255,
-                   x_offset=0, y_offset=0):
+                   x_offset=0, y_offset=0, **kwargs):
         """Add rivers to a PIL image object
 
         :Parameters:
@@ -645,15 +655,15 @@ class ContourWriterCairo(ContourWriterBase):
             Pixel offset in y direction
         """
 
-        self._add_feature(img_ctx, area_def, 'line', 'WDBII', tag='river',
+        self._add_feature(img_surf, area_def, 'line', 'WDBII', tag='river',
                           zero_pad=True, resolution=resolution, level=level,
                           outline=outline, width=width,
                           outline_opacity=outline_opacity, x_offset=x_offset,
-                          y_offset=y_offset)
+                          y_offset=y_offset, **kwargs)
 
     def add_rivers_to_file(self, filename, area_def, resolution='c', level=1,
                            outline='white', width=1, outline_opacity=255,
-                           x_offset=0, y_offset=0):
+                           x_offset=0, y_offset=0, **kwargs):
         """Add rivers to an image file
 
         :Parameters:
@@ -679,12 +689,12 @@ class ContourWriterCairo(ContourWriterBase):
             Pixel offset in y direction
         """
 
-        img_ctx = _read_image(filename)
-        self.add_rivers(img_ctx, area_def, resolution=resolution, level=level,
+        img_surf = _read_image(filename)
+        self.add_rivers(img_surf, area_def, resolution=resolution, level=level,
                         outline=outline, width=width,
                         outline_opacity=outline_opacity, x_offset=x_offset,
                         y_offset=y_offset)
-        _save_image(filename, img_ctx)
+        _save_image(filename, img_surf)
 
     def _get_font(self, outline, font_file, font_size):
         """Return a font."""
@@ -717,33 +727,46 @@ class ContourWriterCairo(ContourWriterBase):
 
 
 def _pil_to_cairo(pil_img):
-    '''Convert PIL image to cairo context'''
-    img_rgba = np.array(pil_img.convert('RGBA'))
-    data = np.array(img_rgba.tostring('raw', 'BGRA'))
+    '''Convert PIL image to cairo surface'''
+
+    img_rgba = pil_img.convert('RGBA')
+    data = np.array(img_rgba.tobytes('raw', 'BGRA'))
     width, height = img_rgba.size
     surface = ImageSurface.create_for_data(data, FORMAT_ARGB32,
                                            width, height)
-    return Context(surface)
+    return surface
 
 
-def _cairo_to_pil(img_ctx, out_mode='RGB'):
+def _cairo_to_pil(surface, out_mode='RGB'):
     '''Convert Cairo image context to PIL image.'''
     # Finalize the image to fix changes
 
-    cairo_format = img_ctx.get_format()
+    cairo_format = surface.get_format()
     if cairo_format == FORMAT_ARGB32:
         pil_mode = 'RGB'
         # Cairo has ARGB. Convert this to RGB for PIL which supports
         # only RGB or RGBA.
-        argb_array = np.fromstring(img_ctx.get_data(), 'c').reshape(-1, 4)
+        argb_array = np.fromstring(surface.get_data(), 'c').reshape(-1, 4)
         rgb_array = argb_array[:, 2::-1]
         pil_data = rgb_array.reshape(-1).tostring()
     else:
         raise ValueError('Unsupported cairo format: %d' % cairo_format)
     pil_image = Image.frombuffer(pil_mode,
-                                 (img_ctx.get_width(), img_ctx.get_height()),
+                                 (surface.get_width(), surface.get_height()),
                                  pil_data, "raw", pil_mode, 0, 1)
     return pil_image.convert(out_mode)
+
+
+def get_rgb_values(param, colormap):
+    # Convert color RGB values to be between 0 and 1:
+    if type(param) is str:
+        if param not in colormap:
+            logger.warning("Color %s not supported", param)
+        r, g, b = colormap.get(param, (255, 255, 255))
+    else:
+        r, g, b = param
+
+    return r / 255., g / 255., b / 255.
 
 
 def _read_image(fname):
@@ -752,7 +775,7 @@ def _read_image(fname):
     return _pil_to_cairo(img)
 
 
-def _save_image(img_ctx, fname, out_mode='RGB'):
+def _save_image(fname, img_surf, out_mode='RGB'):
     """Save the given cairo context to a file."""
-    img = _cairo_to_pil(img_ctx, out_mode)
+    img = _cairo_to_pil(img_surf, out_mode)
     img.save(fname)
