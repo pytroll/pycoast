4ff66393eff5ce6f0ea1573ebf1edabfa5a65cd0
Panu Lahtinen <pnuu+git@iki.fi>
2017-05-11 12:46:07 +0300

    Add missing module name from Proj() call

--- pycoast/cw_base.py
+++ pycoast/cw_base.py
@@ -743,7 +743,7 @@ class ContourWriterBase(object):
         foreground = Image.new('RGBA', (x_size, y_size), (0, 0, 0, 0))
 
         # Lines (coasts, rivers, borders) management
-        prj = Proj(area_def.proj4_string)
+        prj = pyproj.Proj(area_def.proj4_string)
         if prj.is_latlong():
             x_ll, y_ll = prj(area_def.area_extent[0], area_def.area_extent[1])
             x_ur, y_ur = prj(area_def.area_extent[2], area_def.area_extent[3])
