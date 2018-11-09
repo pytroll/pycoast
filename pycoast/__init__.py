1986148c7c89b2794235aa7ce1a297e19cd23e41
David Hoese <david.hoese@ssec.wisc.edu>
2018-11-09 16:10:03 -0600

    Switch to using versioneer for version numbers

--- pycoast/__init__.py
+++ pycoast/__init__.py
@@ -1,7 +1,10 @@
 
 # Import all the classes so that usage stays the same as previously.
 
-from .version import __version__
 from .cw_pil import ContourWriter as ContourWriterPIL
 from .cw_agg import ContourWriterAGG
 ContourWriter = ContourWriterAGG
+
+from .version import get_versions
+__version__ = get_versions()['version']
+del get_versions
