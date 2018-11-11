46576a6c4a95e1e15e7f036c53832229796ccf3c
David Hoese <david.hoese@ssec.wisc.edu>
2018-11-11 08:14:06 -0600

    Add AUTHORS.md list

--- pycoast/cw_pil.py
+++ pycoast/cw_pil.py
@@ -1,18 +1,13 @@
 #!/usr/bin/env python
 # -*- coding: utf-8 -*-
-# pycoast, Writing of coastlines, borders and rivers to images in Python
+# pycoast, Writing of coastlines, borders, and rivers to images in Python
 #
-# Copyright (C) 2011-2015
-#    Esben S. Nielsen
-#    Hróbjartur Þorsteinsson
-#    Stefano Cerino
-#    Katja Hungershofer
-#    Panu Lahtinen
+# Copyright (C) 2011-2018 PyCoast Developers
 #
 # This program is free software: you can redistribute it and/or modify
 # it under the terms of the GNU General Public License as published by
 # the Free Software Foundation, either version 3 of the License, or
-#(at your option) any later version.
+# (at your option) any later version.
 #
 # This program is distributed in the hope that it will be useful,
 # but WITHOUT ANY WARRANTY; without even the implied warranty of
