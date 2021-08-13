b0a7c9841713ac3f8f13fceb8911a7773096a4f6
David Hoese <david.hoese@ssec.wisc.edu>
2021-08-13 07:13:10 -0500

    Use sha256 for dict hashing to make deepcode happy

--- pycoast/cw_base.py
+++ pycoast/cw_base.py
@@ -61,7 +61,7 @@ def get_resolution_from_area(area_def):
 
 
 def hash_dict(dict_to_hash: dict) -> str:
-    dhash = hashlib.sha1()
+    dhash = hashlib.sha256()
     encoded = json.dumps(dict_to_hash, sort_keys=True).encode()
     dhash.update(encoded)
     return dhash.hexdigest()
