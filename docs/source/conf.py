3605258b18cc62cd1e39ee70c46118603860954f
Antonio Valentino <antonio.valentino@tiscali.it>
2022-11-01 09:25:28 +0100

    Fix sphinx build in source dir

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -16,12 +16,12 @@ from __future__ import annotations
 import os
 import sys
 
-from pycoast import __version__
-
 # If extensions (or modules to document with autodoc) are in another directory,
 # add these directories to sys.path here. If the directory is relative to the
 # documentation root, use os.path.abspath to make it absolute, like shown here.
 sys.path.insert(0, os.path.abspath("../../"))
+
+from pycoast import __version__
 
 # -- General configuration -----------------------------------------------------
 
