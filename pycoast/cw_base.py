400c85fc818cb00904ff36300f45a9b5854669f4
Antonio Valentino <antonio.valentino@tiscali.it>
2023-07-09 18:35:30 +0200

    Drop support for textsize completely

--- pycoast/cw_base.py
+++ pycoast/cw_base.py
@@ -167,11 +167,8 @@ class ContourWriterBase(object):
 
     def _draw_text(self, draw, position, txt, font, align="cc", **kwargs):
         """Draw text with agg module."""
-        if hasattr(draw, "textsize"):
-            txt_width, txt_height = draw.textsize(txt, font)
-        else:
-            left, top, right, bottom = draw.textbbox(position, txt, font)
-            txt_width, txt_height = right - left, top - bottom
+        left, top, right, bottom = draw.textbbox(position, txt, font)
+        txt_width, txt_height = right - left, top - bottom
         x_pos, y_pos = position
         ax, ay = align.lower()
         if ax == "r":
